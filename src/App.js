d1792f4055014abb491d20609860e6d32559705c
Razvan Timis <30390122+razvantimis@users.noreply.github.com>
2021-01-11 13:24:26 +0200

    Add img

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./styles.css";
+import rockImg from "./assets/rock.png";
+import paperImg from "./assets/paper.png";
+import scissorsImg from "./assets/scissors.png";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <img src={rockImg} alt="rock"></img>
+      <img src={paperImg} alt="paper"></img>
+      <img src={scissorsImg} alt="scissors"></img>
     </div>
   );
 }
